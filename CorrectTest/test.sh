f40c8370e287a30fbad1c16dfac038a7c3bcb8f9
Kasper Myrtye <jpeg1992@gmail.com>
2015-05-14 16:02:45 +0200

    test re done

--- CorrectTest/test.sh
+++ CorrectTest/test.sh
@@ -2,26 +2,52 @@ echo "-----------------------------------------------------"
 echo "Sequence tests"
 echo "-----------------------------------------------------"
 
-echo "./scanfm -p   -d sq1d.txt"
+echo "empty sequence"
 ./scanfm -p   -d sq1d.txt
-echo "./scanfm -p A -d sq2d.txt"
+echo "A"
 ./scanfm -p A -d sq2d.txt
-echo "./scanfm -p ACCGT -d sq3d.txt"
+echo "ACCGT"
 ./scanfm -p ACCGT -d sq3d.txt
-echo "./scanfm -p ACC[1,0,0] -d sq4d.txt"
-./scanfm -p ACC[1,0,0] -d sq4d.txt
-echo "./scanfm -p ATG[0,1,0] -d sq5d.txt"
-./scanfm -p ATG[0,1,0] -d sq5d.txt
-echo "./scanfm -p ACC[0,0,1] -d sq6d.txt"
-./scanfm -p ACC[0,0,1] -d sq6d.txt
-echo "./scanfm -p ACTTT[1,2,0] -d sq7d.txt"
-./scanfm -p ACTTT[1,2,0] -d sq7d.txt
-echo "./scanfm -p ATTCCCTT[2,2,1] -d sq8d.txt"
-./scanfm -p ATTCCCTT[2,2,1] -d sq8d.txt
-echo "./scanfm -p R -d sq9d.txt"
-./scanfm -p R -d sq9d.txt
-echo "./scanfm -p RRGGWW -d sq10d.txt"
-./scanfm -p RRGGWW -d sq10d.txt
+echo "ACGT[0,0,0]"
+./scanfm -p ACGT[0,0,0] -d sq5d.txt
+echo "ACC[1,0,0]"
+./scanfm -p ACC[1,0,0] -d sq5d.txt
+echo "ATG[0,1,0]"
+./scanfm -p ATG[0,1,0] -d sq6d.txt
+echo "ACC[0,0,1]"
+./scanfm -p ACC[0,0,1] -d sq7d.txt
+echo "AGGT[1,1,0]"
+./scanfm -p AGGT[1,1,0] -d sq8d.txt
+echo "ACGT[1,0,1]"
+./scanfm -p ACGT[1,0,1] -d sq9d.txt
+echo "ATTGT[0,1,1]"
+./scanfm -p ATTGT[0,1,1] -d sq10d.txt
+echo "AACGT[1,1,1]"
+./scanfm -p AACGT[1,1,1] -d sq11d.txt
+echo "ACCT[2,0,0]"
+./scanfm -p ACCT[2,0,] -d sq12d.txt
+echo "AGTTT[0,2,0]"
+./scanfm -p AGTTT[0,2,0] -d sq13d.txt
+echo "ACGT[0,0,2]"
+./scanfm -p ACGT[0,0,2] -d sq14d.txt
+echo "ACTTT[1,2,0]"
+./scanfm -p ACTTT[1,2,0] -d sq15d.txt
+echo "TCGAT[3,0,2]"
+./scanfm -p TCGAT[3,0,2] -d sq16d.txt
+echo "TCGGT[0,2,3]"
+./scanfm -p TCGGT[0,2,3] -d sq17d.txt
+echo "ATTCCCTT[2,2,1]"
+./scanfm -p ATTCCCTT[2,2,1] -d sq18d.txt
+echo "ATTC[0,6,0]"
+./scanfm -p ATTC[0,6,0] -d sq19d.txt
+echo "ATTCG[8,0,0]"
+./scanfm -p ATTCG[8,0,0] -d sq20d.txt
+echo "R"
+./scanfm -p R -d sq21d.txt
+echo "RRGGWW"
+./scanfm -p RRGGWW -d sq22d.txt
+echo "UUMMRRWWSSYYKKBBBDDDHHHVVVNNNN"
+./scanfm -p UUMMRRWWSSYYKKBBBDDDHHHVVVNNNN -d sq23d.txt
 
 
 echo "-----------------------------------------------------"
