fbcd051fad07e3acf16c4de6ebd57d570c7bbdc0
Kasper Myrtue <jpeg@Kadabra.(none)>
2015-05-21 16:18:56 +0200

    Correctness test and clean code

--- CorrectTest/test.sh
+++ CorrectTest/test.sh
@@ -42,13 +42,25 @@ echo "ATTC[0,6,0]"
 ./scanfm -p ATTC[0,6,0] -d sq19d.txt
 echo "ATTCG[8,0,0]"
 ./scanfm -p ATTCG[8,0,0] -d sq20d.txt
+echo "-----------------------------------------------------"
+echo "ambiguous"
+echo "-----------------------------------------------------"
 echo "R"
 ./scanfm -p R -d sq21d.txt
 echo "RRGGWW"
 ./scanfm -p RRGGWW -d sq22d.txt
-echo "UUMMRRWWSSYYKKBBBDDDHHHVVVNNNN"
-./scanfm -p UUMMRRWWSSYYKKBBBDDDHHHVVVNNNN -d sq23d.txt
+echo "UMMRRWWSSYYKKBBBDDDHHHVVVNNNN"
+./scanfm -p MMRRWWSSYYKKBBBDDDHHHVVVNNNN -d sq23d.txt
 
+./scanfm -p UMMRRWWSSYYKKBBBDDDHHHVVVNNNN -d sq23d.txt
+
+echo "-----------------------------------------------------"
+echo "ambiguous mix"
+echo "-----------------------------------------------------"
+echo "AM"
+./scanfm -p AM -d sq24d.txt
+echo "TTATMTNYY"
+./scanfm -p TTATMTNYY -d sq25d.txt
 
 echo "-----------------------------------------------------"
 echo "Range tests"
@@ -58,51 +70,102 @@ echo "./scanfm -p 0...0 -d ra1d.txt"
 echo "failed resulting in never ending loop"
 echo "./scanfm -p 0...1 -d ra2d.txt"
 echo "failed resulting in never ending loop"
+echo "1...1"
+./scanfm -p 1...1 -d ra3d.txt
 echo "./scanfm -p 4...9 -d ra3d.txt"
-./scanfm -p 4...9 -d ra3d.txt
+./scanfm -p 4...9 -d ra4d.txt
 
 
 echo "-----------------------------------------------------"
 echo "Reference tests"
 echo "-----------------------------------------------------"
 
-echo "./scanfm -p p1=0...0 2...4 p1 -d re1d.txt"
+echo "p1=0...0 2...4 p1"
 ./scanfm -p p1=0...0 2...4 p1 -d re1d.txt
-echo "./scanfm -p p1=1...1 2...2 p1 -d re2d.txt"
+echo "p1=1...1 2...2 p1"
 ./scanfm -p p1=1...1 2...2 p1 -d re2d.txt
-echo "./scanfm -p p1=3...5 2...2 p1 -d re3d.txt"
+echo "p1=3...5 2...2 p1"
 ./scanfm -p p1=3...5 2...2 p1 -d re3d.txt
-echo "./scanfm -p p1=3...5 p1 -d re4d.txt"
-./scanfm -p p1=3...5 p1 -d re4d.txt
-echo "./scanfm -p p1=0...0 2...2 ~p1 -d re5d.txt"
-./scanfm -p p1=0...0 2...2 ~p1 -d re5d.txt
-echo "./scanfm -p p1=1...1 2...2 ~p1 -d re6d.txt"
-./scanfm -p p1=1...1 2...2 ~p1 -d re6d.txt
-echo "./scanfm -p p1=3...5 2...2 ~p1 -d re7d.txt"
-./scanfm -p p1=3...5 2...2 ~p1 -d re7d.txt
-echo "./scanfm -p p1=3...3 ~p1 -d re8d.txt"
-./scanfm -p p1=3...3 ~p1 -d re8d.txt
-echo "./scanfm -p p1=3...4 2...2 p1[2,1,1] -d re9d.txt"
-./scanfm -p p1=3...4 2...2 p1[2,1,1] -d re9d.txt
-echo "./scanfm -p p1=1...1 2...3 ~p1[0,1,0] -d re10d.txt"
-./scanfm -p p1=1...1 2...3 ~p1[0,1,0] -d re10d.txt
-echo "./scanfm -p p1=3...4 2...3 ~p1[1,0,1] -d re11d.txt"
-./scanfm -p p1=3...4 2...3 ~p1[1,0,1] -d re11d.txt
-echo "./scanfm -p p1=3...4 ~p1[1,0,0] -d re12d.txt"
-./scanfm -p p1=3...4 ~p1[1,0,0] -d re12d.txt
+echo "p1=0...0 p1"
+./scanfm -p p1=0...0 p1 -d re4d.txt
+echo "p1=1...1 p1"
+./scanfm -p p1=1...1 p1 -d re5d.txt
+echo "p1=3...5 p1"
+./scanfm -p p1=3...5 p1 -d re6d.txt
+
+
+
+echo "-----------------------------------------"
+echo "complementary reference"
+echo "-----------------------------------------"
+echo "p1=0...0 2...2 ~p1"
+./scanfm -p p1=0...0 2...2 ~p1 -d re7d.txt
+echo "p1=1...1 2...2 ~p1"
+./scanfm -p p1=1...1 2...2 ~p1 -d re8d.txt
+echo "p1=3...5 2...2 ~p1"
+./scanfm -p p1=3...5 2...2 ~p1 -d re9d.txt
+echo "p1=0...0 ~p1"
+./scanfm -p p1=0...0 ~p1 -d re10d.txt
+echo "p1=1...1 ~p1"
+./scanfm -p p1=1...1 ~p1 -d re11d.txt
+echo "p1=3...3 ~p1"
+./scanfm -p p1=3...3 ~p1 -d re12d.txt
+
+echo "-----------------------------------------------------"
+echo "MID reference"
+echo "-----------------------------------------------------"
+
+echo "p1=0...0 2...2 p1[1,2,1]"
+./scanfm -p p1=0...0 2...2 p1[1,2,1] -d re13d.txt
+echo "p1=1...1 2...3 p1[1,0,0]"
+./scanfm -p p1=1...1 2...2 p1[1,0,0] -d re14d.txt
+echo "p1=3...4 2...2 p1[2,1,1]"
+./scanfm -p p1=3...4 2...2 p1[2,1,1] -d re15d.txt
+echo "p1=0...0 p1[1,1,1]"
+./scanfm -p p1=0...0 p1[1,1,1] -d re16d.txt
+echo "p1=1...1 p1[2,1,2]"
+./scanfm -p p1=1...1 p1[2,1,2] -d re17d.txt
+echo "p1=3...5 p1[2,1,1]"
+./scanfm -p p1=3...5 p1[2,1,1] -d re18d.txt
+
+echo "-----------------------------------------"
+echo "complementary MID reference"
+echo "-----------------------------------------"
+
+echo "p1=0...0 2...3 ~p1[1,2,1]"
+./scanfm -p p1=0...0 2...3 ~p1[1,2,1] -d re19d.txt
+echo "p1=1...1 2...3 ~p1[0,1,0]"
+./scanfm -p p1=1...1 2...3 ~p1[0,1,0] -d re20d.txt
+echo "p1=3...4 2...3 ~p1[1,0,1]"
+./scanfm -p p1=3...4 2...3 ~p1[1,0,1] -d re21d.txt
+echo "p1=0...0 ~p1[0,1,2]"
+./scanfm -p p1=0...0 ~p1[0,1,2] -d re22d.txt
+echo "p1=1...1 ~p1[0,0,2]"
+./scanfm -p p1=1...1 ~p1[0,0,2] -d re23d.txt
+echo "p1=3...4 ~p1[1,0,0]"
+./scanfm -p p1=3...4 ~p1[1,0,0] -d re24d.txt
 
 
 echo "-----------------------------------------------------"
 echo "Multiple PU's tests"
 echo "-----------------------------------------------------"
 
-echo "./scanfm -p ACGT TGCA -d mu1d.txt"
+echo "ACGT TGCA"
 ./scanfm -p ACGT TGCA -d mu1d.txt
-echo "./scanfm -p 4...5 TTTT[1,1,1] -d mu2d.txt"
+echo "4...5 TTTT[1,1,1]"
 ./scanfm -p 4...5 TTTT[1,1,1] -d mu2d.txt
-echo "./scanfm -p TTTT[0,1,1] TTA -d mu3d.txt"
+echo "TTTT[0,1,1] TTA"
 ./scanfm -p TTTT[0,1,1] TTA -d mu3d.txt
-echo "./scanfm -p p1=2...3 p2=2...2 p1 p2 -d mu4d.txt"
+echo "p1=2...3 p2=2...2 p1 p2"
 ./scanfm -p p1=2...3 p2=2...2 p1 p2 -d mu4d.txt
-echo "./scanfm -p p1=3...3 p2=2...3 2...3 p2 p1 -d mu5d.txt"
-./scanfm -p p1=3...3 p2=2...3 2...3 ~p2 ~p1 -d mu5d.txt
+echo "p1=3...3 p2=2...3 2...3 ~p2 ~p1"
+./scanfm -p p1=3...3 p2=2...3 2...3 ~p2 ~p1 -d mu5d.txt 
+echo "AATCC p1=0...1 0...3 p1 TTG"
+echo "./scanfm -p AATCC p1=0...1 0...3 p1 TTG -d mu6d.txt"
+echo "never ending loop"
+echo "ATC 0...3 TTC"
+./scanfm -p ATC 0...3 TTC -d mu7d.txt
+echo "p1=2...4 ATTT p1[0,6,0] TTT"
+./scanfm -p p1=2...4 ATTT p1[0,6,0] TTT -d mu8d.txt
+echo "TTTT ACC[0,6,0] TT"
+./scanfm -p TTTT ACC[0,6,0] TT -d mu9d.txt
